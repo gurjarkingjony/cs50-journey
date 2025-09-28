1416477eccc2a3f1d74ff41b143e058be2d246eb
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-09-28 16:31:44 +0100

    cs50 👾🚀

--- readability/readability.c
+++ readability/readability.c
@@ -5,42 +5,34 @@
 #include <math.h>
 int main(void)
 {
-    // Prompt user
     string text = get_string("Text: ");
 
-    // String length
     int i = strlen(text);
     int letters = 0;
     int words = 0;
     int sentences = 0;
 
-    // Loop for counting
     for (int x = 0; x < i; x++)
     {
-        // Counting letters
         char c = text[x];
         if (isalpha(c) != 0)
         {
             letters++;
         }
 
-        // Counting words
         if (c == ' ')
         {
             words++;
         }
 
-        // Counting Sentences
         if (c == '.' || c == '!' || c == '?')
         {
             sentences++;
         }
     }
 
-    // To account for last word
     words = words + 1;
 
-    // Calculation
     float L = ((float)letters / (float)words) * 100;
     float s = ((float)sentences / (float)words) * 100;
     float subindex = 0.0588 * L - 0.296 * s - 15.8;
