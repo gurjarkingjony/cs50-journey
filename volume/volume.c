9b20857d5d75249d8f338edb6bc4fb1c8eaf25d2
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-09-28 16:37:22 +0100

    cs50 👾🚀

--- volume/volume.c
+++ volume/volume.c
@@ -4,19 +4,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
 int main(int argc, char *argv[])
 {
-    // Check command-line arguments
     if (argc != 4)
     {
         printf("Usage: ./volume input.wav output.wav factor\n");
         return 1;
     }
 
-    // Open files and determine scaling factor
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
     {
@@ -33,13 +30,11 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     /* There is only one header, so parse it just once */
     fread(&header, sizeof(header), 1, input);
     fwrite(&header, sizeof(header), 1, output);
 
-    // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
     while (fread(&buffer, sizeof(buffer), 1, input))
     {
@@ -47,7 +42,6 @@ int main(int argc, char *argv[])
         fwrite(&buffer, sizeof(buffer), 1, output);
     }
 
-    // Close files
     fclose(input);
     fclose(output);
 }
